161dacb16bc29f765f063f5d913c68504ea70dd1
Mohamed Nasser <mhmeeaad@gmail.com>
2018-04-24 01:03:49 +0200

    make nmjla exportable, now it returns a function that return a promise.

--- nmjla.js
+++ nmjla.js
@@ -12,76 +12,63 @@ const id = /[a-zA-z]\w*/g;
 
 // helper function
 const splitAt = index => x => [x.slice(0, index), x.slice(index)];
+module.exports = path =>
+  new Promise((resolve, reject) => {
+    rw.getFileContent(path, str => {
+      let allTokens = [];
+      // collect types
+      types.forEach(e => {
+        while ((match = e.reg.exec(str))) {
+          let matched = match[2] ? match[2].toString() : match.toString();
+          allTokens.push({ index: match.index, type: e.name, token: matched });
+          str = splitAt(match.index)(str);
+          str[1] = str[1].replace(matched, " ".repeat(matched.length));
+          str = str.join("");
+        }
+      });
 
-let tests = [
-  'class class1 {\n	int value = 3;\n	String text = "int x = 1";\n	/*\n		if (num < 1)\n            	num = num +1 ;\n	*/\n	private void print()	\n	{\n		System.out.println("/*hello*/");	\n	}\n};',
-  "class Factorial{\npublic static void main(String[] a){ System.out.println(new Fac().ComputeFac(10));\n}\n}\n\nclass Fac {\npublic int ComputeFac(int num){\nint num_aux ;\nif (num < 1)\nnum_aux = 1 ; \nelse\n \n\nnum_aux = num * (this.ComputeFac(num+1)) ; \nreturn num_aux ;\n}\n}",
-  "class myclass{\n/* my ultimate comment */\nint var)) XX1 = 0; */\nint 3CC1) = 0; */\n10+5;\n};"
-];
+      // collect reserved words
+      reservedWords.all.forEach(e => {
+        while ((match = e.reg.exec(str))) {
+          allTokens.push({
+            index: match.index,
+            type: e.name,
+            token: match[2].toString()
+          });
+        }
+      });
 
-let testPaathes = [ 'TESTCASES/TESTCASE1.txt' , 'TESTCASES/TESTCASE2.txt' , 'TESTCASES/TESTCASE3.txt'].forEach(path => {
-  // the source code
-  rw.getFileContent(path, str => {
-    let allTokens = [];
+      // collect operations
+      operations.forEach(e => {
+        while ((match = e.reg.exec(str))) {
+          allTokens.push({
+            index: match.index,
+            type: e.name,
+            token: match.toString()
+          });
+        }
+      });
 
-    console.log(
-      "\n||||||||||||||||||||||| TEST ||||||||||||||||||||||||||||||||||\n"
-    );
-
-    // collect types
-    types.forEach(e => {
-      while ((match = e.reg.exec(str))) {
-        let matched = match[2] ? match[2].toString() : match.toString();
-        allTokens.push({ index: match.index, type: e.name, token: matched });
-        str = splitAt(match.index)(str);
-        str[1] = str[1].replace(matched, " ".repeat(matched.length));
-        str = str.join("");
+      // collect ids
+      while ((match = id.exec(str))) {
+        if (
+          new RegExp(reservedWords.sum, "g").exec(match.toString()).index != 1
+        ) {
+          allTokens.push({
+            index: match.index,
+            type: "ID",
+            token: match[0].toString()
+          });
+        }
       }
+
+      allTokens.sort((a, b) => a.index - b.index);
+      
+      resolve(allTokens);
+      
+      rw.saveFile(
+        "RESULTS/" + path.split("/")[1],
+        allTokens.map(e => "< " + e.type + " > : -" + e.token + "-").join("\n")
+      );
     });
-
-    // collect reserved words
-    reservedWords.all.forEach(e => {
-      while ((match = e.reg.exec(str))) {
-        allTokens.push({
-          index: match.index,
-          type: e.name,
-          token: match[2].toString()
-        });
-      }
-    });
-
-    // collect operations
-    operations.forEach(e => {
-      while ((match = e.reg.exec(str))) {
-        allTokens.push({
-          index: match.index,
-          type: e.name,
-          token: match.toString()
-        });
-      }
-    });
-
-    // collect ids
-    while ((match = id.exec(str))) {
-      if (
-        new RegExp(reservedWords.sum, "g").exec(match.toString()).index != 1
-      ) {
-        allTokens.push({
-          index: match.index,
-          type: "ID",
-          token: match[0].toString()
-        });
-      }
-    }
-
-    allTokens.sort((a, b) => a.index - b.index);
-
-    allTokens.forEach(e => {
-      console.log("< " + e.type + " > : \n" + e.token);
-      console.log("----------------------------------");
-    });
-
-    rw.saveFile( 'RESULTS/'+path.split('/')[1] , allTokens.map( e => "< " + e.type + " > : -" + e.token + '-' ).join('\n') );
-
   });
-});
